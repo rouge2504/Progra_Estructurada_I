02874e8534c50444937024c0c049943b96f3d2bc
Rogelio Trejo <rouge2504@gmail.com>
2022-08-03 12:29:01 -0500

    Set Element

--- Assets/Scripts/Medabot.cs
+++ Assets/Scripts/Medabot.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Create Medabot", menuName = "Medabot")]
 public class Medabot : ScriptableObject
 {
+    public enum ELEMENT { NONE, FIRE, GRASS, WATER, ELECTRIC, WIND, LIGHT, DARK}
+
     public int velocity;
     public int life;
     public int force;
     public int energy;
     public int defense;
     public int stamina;
+
+    public ELEMENT element = ELEMENT.NONE;
 }
